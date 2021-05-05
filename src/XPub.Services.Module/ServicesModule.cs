1f62c08b77452fbe6d785a46612a3c7991e0d7b9
stevensrf11 <stevensrf@mail.com>
2021-05-05 09:01:41 -0500

    Add files via upload

---
+++ src/XPub.Services.Module/ServicesModule.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using XPub.Infrastructure.Prism;
+using XPub.Interfaces.Services.Administration;
+using XPub.Services.Administration;
+using XPub.Services.Module.Views;
+
+namespace XPub.Services.Module
+{
+    [Module(ModuleName = "XPub.Services.Module.ServiceModule")]
+    public class ServicesModule :  ModuleBase
+    {
+        public override void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<IXPubServiceAdmin, XPubServiceAdmin>();
+        }
+    }
+}
